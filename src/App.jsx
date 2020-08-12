2007c3035da2dc306409d34b4dd1c36a8e2cefb5
Vinicius Ribeiro <vinicius.bezerra@al.resilia.com.br>
2020-08-12 19:35:04 -0300

    Mobile First

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react' ;
 
 
+import Landing from '../src/pages/Landing/index'
+
+import './assets/styles/global.css';
+
+
 function App() {
     return (
-        <div className="App">
-          <h1>Hello World</h1>
-        </div>
+     <Landing />
     );
 }
 
